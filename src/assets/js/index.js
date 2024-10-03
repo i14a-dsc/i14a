efa0c0aed20c3626e5a21c6d48375cb3cbce79c9
i14a-dsc <onewsacount@gmail.com>
2024-10-03 18:02:21 +0900

    add open discord button

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   console.log(`loaded in ${Date.now() - performance.timing.navigationStart}ms`);
+
+  const dialog_open = document.querySelector('#dialog_open');
+  const dialog_close = document.querySelector('#dialog_close');
+
+  dialog_open.addEventListener('click', () => {
+    document.querySelector('#dialog').setAttribute('open', '');
+  });
+
+  dialog_close.addEventListener('click', () => {
+    document.querySelector('#dialog').setAttribute('open', 'false');
+  });
 });
